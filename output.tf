1f1248b23e7b2e40691801c2ee996385fed045b2
ankitrm <passw0rd>
2019-03-17 22:23:55 +0800

    Add SSM Policy ARN

--- output.tf
+++ output.tf
@@ -8,6 +8,11 @@ output "iam_policy_name" {
   description = "the name of iam policy for allowing session manager access"
 }
 
+output "iam_policy_arn" {
+  value       = "${aws_iam_policy.this.arn}"
+  description = "the name of iam policy for allowing session manager access"
+}
+
 output "ssm_document_name" {
   value       = "${local.ssm_document_name}"
   description = "the name of ssm document for session manager preferences"
