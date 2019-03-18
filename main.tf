b6130416258fc17863d4cc78d427121a7d426bf9
steve.ng <steveng.1988@gmail.com>
2019-03-18 14:39:10 +0800

    use resource as bucket to create terraform dependency

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  bucket = "${local.s3_bucket_name}"
+  bucket = "${aws_s3_bucket.this.id}"
   policy = "${data.aws_iam_policy_document.s3_bucket.json}"
 }
 
