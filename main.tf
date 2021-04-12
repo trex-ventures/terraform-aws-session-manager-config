13058ca49e8b2810cca0cd7db4ca00ebc4259df5
Ayush Choubey <ayush.choubey@traveloka.com>
2021-04-12 12:34:19 +0800

    make ssm logs bucket private (#16)

--- main.tf
+++ main.tf
@@ -27,6 +27,15 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.s3_bucket.json
